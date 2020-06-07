56a35d133e98b5e93704f04a17b261ffbbe08712
VigneshwaranRavi23 <vickyimag@gmail.com>
2020-06-07 20:32:51 +0530

    Updates

---
+++ precache-manifest.3a4ae8c8c2aac8d36217a93e219e3899.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f23ae65fb42d7d8fe1774a84dbd5f4d0",
+    "url": "/covid19-tracker/index.html"
+  },
+  {
+    "revision": "e5f148e705ca5b90efcd",
+    "url": "/covid19-tracker/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "110c226d1151ff8ae057",
+    "url": "/covid19-tracker/static/css/main.9eae5f11.chunk.css"
+  },
+  {
+    "revision": "e5f148e705ca5b90efcd",
+    "url": "/covid19-tracker/static/js/2.872a3c86.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid19-tracker/static/js/2.872a3c86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "110c226d1151ff8ae057",
+    "url": "/covid19-tracker/static/js/main.bfe51db0.chunk.js"
+  },
+  {
+    "revision": "3763e565e5102338e2a4",
+    "url": "/covid19-tracker/static/js/runtime-main.12aeb54c.js"
+  }
+]);
